a77955d051e3e1022812531f58c2c0a83fc6d616
Hugo Oyola <hugo.ham@outlook.com>
2024-04-17 18:30:51 -0500

    Actualizar función editarCliente en API.js

--- js/editarcliente.js
+++ js/editarcliente.js
@@ -1,4 +1,5 @@
-import { obtenerCliente } from "./API.js";
+import { obtenerCliente, editarCliente } from "./API.js";
+import { mostrarAlerta, validar } from './funciones.js';
 
 (function () {
   // Campos del formulario
@@ -16,6 +17,11 @@ import { obtenerCliente } from "./API.js";
     const cliente = await obtenerCliente(idCliente);
 
     mostrarCliente(cliente);
+
+    // Submit al formulario
+    const formulario = document.querySelector("#formulario");
+    formulario.addEventListener("submit", validarCliente);
+
   });
 
   function mostrarCliente(cliente) {
@@ -26,5 +32,25 @@ import { obtenerCliente } from "./API.js";
     emailInput.value = email;
     telefonoInput.value = telefono;
     idInput.value = id;
+  }
+
+  function validarCliente(e) {
+    e.preventDefault();
+
+    const cliente = {
+      nombre: nombreInput.value,
+      email: emailInput.value,
+      empresa: empresaInput.value,
+      telefono: telefonoInput.value,
+      id: idInput.value,
+    };
+
+    if (validar(cliente)) {
+      mostrarAlerta("Todos los campos son obligatorios");
+      return;
+    }
+
+    // Reescribe el objeto
+    editarCliente(cliente);
   }
 })();
